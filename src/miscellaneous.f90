0ce15b1272d5774ce2cef136845b739572e236f7
benbaed <99179689+benbaed@users.noreply.github.com>
2026-03-16 15:10:07 +0100

    Adjusting to d4par and d4file merge

--- src/miscellaneous.f90
+++ src/miscellaneous.f90
@@ -16,9 +16,7 @@ contains
       write(*,'(a35,10x,a)') "--uhf <int>",       "# set number of unpaired electrons"
       write(*,'(a35,10x,a)') "--efield <x> <y> <z>","# set electric field vector"
       write(*,'(a35,10x,a)') "--outname <filename>","# set output file name. NOTE: An'.inp' suffix will be added."
-      write(*,'(a35,10x,a)') "--cm <charge model>", &
-"# set charge model. Options: 'eeq', 'ceh', 'ceh_external', 'ext', " // &
-"'extonlyq', 'extscript'. DEFAULT: 'ceh'"
+      write(*,'(a35,10x,a)') "--cm <charge model>","# set charge model. Options: 'eeq', 'ceh', 'ceh_external', 'ext', 'extonlyq', 'extscript'. DEFAULT: 'ceh'"
       write(*,'(a35,10x,a)') "--extscript <path>","# set path to external script (use with --cm extscript)"
       write(*,'(a5x,a)') " ","# If not provided, reads from ~/.extscript_path"
       write(*,'(a35,10x,a)') "--qvSZPs", &
@@ -33,7 +31,7 @@ contains
       write(*,'(a35,10x,a)') "--nososcf",         "# do not use SOSCF algorithm for SCF convergence"
       write(*,'(a35,10x,a)') "--guess <guess options>","# SCF guess options, see ORCA manual"
       write(*,'(a35,10x,a)') "--d4par <s6> <s8> <a1> <a2> <s9>","# set D4 dispersion parameters"
-      write(*,'(a35,10x,a)') "--d4file","# read D4 dispersion parameters from .d4par file"
+      write(*,'(a35,10x,a)') "--d4par d4file","# read D4 dispersion parameters from .d4par file"
       write(*,'(a35,10x,a)') "--polar",           "# calculate polarizability"
       write(*,'(a35,10x,a)') "--polgrad",         "# calculate polarizability gradient"
       write(*,'(a35,10x,a)') "--dipgrad",         "# calculate dipole moment gradient"
